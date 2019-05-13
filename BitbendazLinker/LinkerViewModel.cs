6600a9e4d1ae2a6eea9cc4eb81d36bf1a9fd936f
MikaelStalvik <mikael.stalvik@gmail.com>
2019-05-13 21:27:21 +0200

    Fixed shader generation

--- BitbendazLinker/LinkerViewModel.cs
+++ BitbendazLinker/LinkerViewModel.cs
@@ -3,6 +3,8 @@ using Microsoft.Win32;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Windows;
 
 namespace BitbendazLinker
 {
@@ -34,8 +36,17 @@ namespace BitbendazLinker
             set => SetProperty(ref _indexFile, value);
         }
 
+        private string _shaderOutputFile;
+        public string ShaderOutputFile
+        {
+            get => _shaderOutputFile;
+            set => SetProperty(ref _shaderOutputFile, value);
+        }
+
         public RelayCommand BrowseCommand { get; }
         public RelayCommand LoadCommand { get; }
+        public RelayCommand GenerateShadersCommand { get; }
+        public RelayCommand BrowseShaderOutputFileCommand { get; }
 
         public LinkerViewModel()
         {
@@ -51,14 +62,59 @@ namespace BitbendazLinker
                 }
             }, o => true);
             LoadCommand = new RelayCommand(LoadFromFile, o => File.Exists(_indexFile));
+            GenerateShadersCommand = new RelayCommand(GenerateShaders, o => !string.IsNullOrWhiteSpace(_shaderOutputFile) );
+            BrowseShaderOutputFileCommand = new RelayCommand(o =>
+            {
+                var dlg = new SaveFileDialog();
+                dlg.DefaultExt = ".h";
+                dlg.Filter = "Header files|*.h";
+                if (dlg.ShowDialog() == true)
+                {
+                    ShaderOutputFile = dlg.FileName;
+                    GenerateShadersCommand.InvokeCanExecuteChanged();
+                }
+            }, o => true);
         }
-        public void LoadFromFile(object o)
+        private void LoadFromFile(object o)
         {
             var json = File.ReadAllText(_indexFile);
             var contentData = JsonConvert.DeserializeObject<ContentData>(json);
             Shaders = contentData.Shaders;
             Objects = contentData.Objects;
             Textures = contentData.Textures;
+        }
+
+        private void GenerateShaders(object o)
+        {
+            if (_shaders.Count == 0)
+            {
+                MessageBox.Show("No shaders selected");
+                return;
+            }
+            var sb = new StringBuilder();
+            sb.AppendLine("namespace {");
+            foreach (var glslFile in _shaders)
+            {
+                var sourceData = File.ReadAllLines(glslFile);
+                var name = Path.GetFileName(glslFile).Replace(".glsl", "_min");
+                sb.AppendLine($"const char *{name} =");
+                var nl = "\\n";
+                foreach (var s in sourceData)
+                {
+                    var ts = s.Trim();
+                    if (!string.IsNullOrEmpty(ts) && !ts.StartsWith("//"))
+                    {
+                        if (!ts.StartsWith(@"//"))
+                        {
+                            sb.AppendLine($"\"{s}{nl}\"");
+                        }
+                    }
+                }
+                sb.Append(";");
+            }
+            sb.AppendLine("}");
+            File.WriteAllText(_shaderOutputFile, sb.ToString());
+            MessageBox.Show("Shader header file generated");
         }
     }
 }
