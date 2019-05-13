e033940dc59e3e1a2ea57107ead89f0d87f49c19
MikaelStalvik <mikael.stalvik@gmail.com>
2019-05-13 20:59:19 +0200

    Initial commit

---
+++ BitbendazLinker/LinkerViewModel.cs
@@ -0,0 +1,64 @@
+﻿using BitbendazLinker.Models;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BitbendazLinker
+{
+    public class LinkerViewModel : ViewModelBase
+    {
+        private List<string> _shaders;
+        public List<string> Shaders
+        {
+            get => _shaders;
+            set => SetProperty(ref _shaders, value);
+        }
+        private List<string> _objects;
+        public List<string> Objects
+        {
+            get => _objects;
+            set => SetProperty(ref _objects, value);
+        }
+        private List<string> _textures;
+        public List<string> Textures
+        {
+            get => _textures;
+            set => SetProperty(ref _textures, value);
+        }
+
+        private string _indexFile;
+        public string IndexFile
+        {
+            get => _indexFile;
+            set => SetProperty(ref _indexFile, value);
+        }
+
+        public RelayCommand BrowseCommand { get; }
+        public RelayCommand LoadCommand { get; }
+
+        public LinkerViewModel()
+        {
+            BrowseCommand = new RelayCommand(o =>
+            {
+                var dlg = new OpenFileDialog();
+                dlg.DefaultExt = ".json";
+                dlg.Filter = "JSON files|*.json";
+                if (dlg.ShowDialog() == true)
+                {
+                    IndexFile = dlg.FileName;
+                    LoadCommand.InvokeCanExecuteChanged();
+                }
+            }, o => true);
+            LoadCommand = new RelayCommand(LoadFromFile, o => File.Exists(_indexFile));
+        }
+        public void LoadFromFile(object o)
+        {
+            var json = File.ReadAllText(_indexFile);
+            var contentData = JsonConvert.DeserializeObject<ContentData>(json);
+            Shaders = contentData.Shaders;
+            Objects = contentData.Objects;
+            Textures = contentData.Textures;
+        }
+    }
+}
---
+++ BitbendazLinker/Models/ContentData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitbendazLinker.Models
+{
+    public class ContentData
+    {
+        public List<string> Shaders { get; set; }
+        public List<string> Objects { get; set; }
+        public List<string> Textures { get; set; }
+    }
+}
