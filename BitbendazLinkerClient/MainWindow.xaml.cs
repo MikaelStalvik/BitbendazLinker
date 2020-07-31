b33c07d51e44aa30e1076492d3efeaffaaed0382
MikaelStalvik <mikael.stalvik@gmail.com>
2020-07-31 12:50:06 +0200

    Custom window theme

--- BitbendazLinkerClient/MainWindow.xaml.cs
+++ BitbendazLinkerClient/MainWindow.xaml.cs
@@ -1,12 +1,11 @@
-﻿using System.Windows;
-using AdonisUI;
+﻿using AdonisUI.Controls;
 
 namespace BitbendazLinkerClient
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : AdonisWindow
     {
 
         public MainWindow()
